fc04105920c539529d378754d16323a19b2ce586
adamhoof <hofmaad2@fel.cvut.cz>
2024-04-02 17:02:04 +0200

    Upgrade ArpCache

--- src/main/java/model/ArpCache.java
+++ src/main/java/model/ArpCache.java
@@ -1,4 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+
 public class ArpCache {
+    ArrayList<ArpEntry> entries = new ArrayList<>();
+
+    public MACAddress getMAC(IPAddress ipAddress) {
+        for (ArpEntry arpEntry : entries) {
+            if (arpEntry.getIpAddress() != ipAddress) {
+                continue;
+            }
+            return arpEntry.getMac();
+        }
+        return null;
+    }
 }
