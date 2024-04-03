6735d93be9b7f76305983ef0d7aa4b65a2ffd7ee
adamhoof <hofmaad2@fel.cvut.cz>
2024-04-03 16:28:47 +0200

    Adopt CAM table to new Switch changes

--- src/main/java/model/CAMTable.java
+++ src/main/java/model/CAMTable.java
@@ -1,4 +1,28 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CAMTable {
+    private Set<MACAddress> entries;
+
+    public CAMTable() {
+        this.entries = new HashSet<>();
+    }
+
+    public void addEntry(MACAddress macAddress) {
+        entries.add(macAddress);
+    }
+
+    public boolean containsEntry(MACAddress macAddress) {
+        return entries.contains(macAddress);
+    }
+
+    public void removeEntry(MACAddress macAddress) {
+        entries.remove(macAddress);
+    }
+
+    public Set<MACAddress> getEntries() {
+        return new HashSet<>(entries);
+    }
 }
