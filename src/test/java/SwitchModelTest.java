b42b980a86d59a245cb42b9742049a7e89aec55b
adamhoof <hofmaad2@fel.cvut.cz>
2024-04-05 12:27:12 +0200

    Enhance SwitchModelTest

--- src/test/java/SwitchModelTest.java
+++ src/test/java/SwitchModelTest.java
@@ -1,4 +1,7 @@
+import common.AutoNameGenerator;
 import model.MACAddress;
+import model.PCModel;
+import model.SwitchConnection;
 import model.SwitchModel;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,17 +10,32 @@ import java.util.UUID;
 
 public class SwitchModelTest {
     @Test
-    public void removeEntry__addEntryPickupCorrectPort() {
-        SwitchModel switchModel = new SwitchModel(UUID.randomUUID(), new MACAddress(UUID.randomUUID().toString()));
+    public void learnMacAddress() {
+        SwitchModel switchModel = new SwitchModel(UUID.randomUUID(), new MACAddress(UUID.randomUUID().toString()), AutoNameGenerator.generateSwitchName());
+        PCModel toRemove = new PCModel(UUID.randomUUID(), new MACAddress(UUID.randomUUID().toString()), AutoNameGenerator.generatePcName());
 
-        MACAddress toRemove = new MACAddress(UUID.randomUUID().toString());
-        switchModel.learnMacAddress(toRemove);
-        switchModel.learnMacAddress(new MACAddress(UUID.randomUUID().toString()));
-        switchModel.forgetMacAddress(toRemove);
+        switchModel.addConnection(toRemove);
+        SwitchConnection switchConnection = switchModel.getSwitchConnections().iterator().next();
+        Assertions.assertNotNull(switchConnection);
 
-        int expected = 1;
-        int actual = switchModel.getCamTable().getEntries().size();
+        switchModel.learnMacAddress(switchConnection.getNetworkDeviceModel().getMacAddress(), switchConnection.getPort());
+        Assertions.assertEquals(1, switchModel.getSwitchConnections().size());
+    }
 
-        Assertions.assertEquals(expected, actual);
+    @Test
+    public void forgetMacAddress() {
+        SwitchModel switchModel = new SwitchModel(UUID.randomUUID(), new MACAddress(UUID.randomUUID().toString()), AutoNameGenerator.generateSwitchName());
+        PCModel toRemove = new PCModel(UUID.randomUUID(), new MACAddress(UUID.randomUUID().toString()), AutoNameGenerator.generatePcName());
+
+        switchModel.addConnection(toRemove);
+        SwitchConnection switchConnection = switchModel.getSwitchConnections().iterator().next();
+        Assertions.assertNotNull(switchConnection);
+
+        switchModel.learnMacAddress(switchConnection.getNetworkDeviceModel().getMacAddress(), switchConnection.getPort());
+
+        switchModel.forgetMacAddress(toRemove.getMacAddress());
+
+        Assertions.assertEquals(1, switchModel.getSwitchConnections().size());
+        Assertions.assertEquals(0, switchModel.getCamTable().getEntries().size());
     }
 }
