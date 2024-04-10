ca5f80a273965b18b743aae340502e85214a6185
adamhoof <hofmaad2@fel.cvut.cz>
2024-04-10 20:22:52 +0200

    Add logger dependency and it's conf file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module cz.cvut.fel.pjv.hofmaad {
     requires javafx.controls;
+    requires java.logging;
+    requires org.slf4j;
+    requires org.apache.logging.log4j;
     exports entry;
     exports model;
     exports controller;
